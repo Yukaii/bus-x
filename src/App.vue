8bd9f7df0b4e152fdd9059ab9033d9afe608a523
Yukai Huang <yukaihuangtw@gmail.com>
2017-06-07 12:05:19 +0800

    Update page structure

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,43 @@
 <template>
   <div id="app">
-    <search-navbar></search-navbar>
+    <div class="page-container">
 
-    <div class="container-fluid content-container">
-      <div class="list-group route-list">
-        <div class="list-group-item list-group-item-action">
-          <div class="route-detail">
-            <div class="route-number">650</div>
-            <div class="route-dest">往國父紀念館</div>
+      <search-navbar></search-navbar>
+
+      <div class="container-fluid content-container">
+        <div class="list-group route-list">
+          <div class="list-group-item list-group-item-action">
+            <div class="route-detail">
+              <div class="route-number">650</div>
+              <div class="route-dest">往國父紀念館</div>
+            </div>
+            <div class="route-name">萬隆</div>
+            <span class="badge badge-default badge-pill badge-warning">3 min</span>
           </div>
-          <div class="route-name">萬隆</div>
-          <span class="badge badge-default badge-pill badge-warning">3 min</span>
-        </div>
-        <div class="list-group-item list-group-item-action">
-          <div class="route-detail">
-            <div class="route-number">53</div>
-            <div class="route-dest">往太原火車站</div>
+          <div class="list-group-item list-group-item-action">
+            <div class="route-detail">
+              <div class="route-number">53</div>
+              <div class="route-dest">往太原火車站</div>
+            </div>
+            <div class="route-name">大明國光路口</div>
+            <span class="badge badge-default badge-pill badge-info">15 min</span>
           </div>
-          <div class="route-name">大明國光路口</div>
-          <span class="badge badge-default badge-pill badge-info">15 min</span>
-        </div>
-        <div class="list-group-item list-group-item-action">
-          <div class="route-detail">
-            <div class="route-number">18</div>
-            <div class="route-dest">往萬華</div>
+          <div class="list-group-item list-group-item-action">
+            <div class="route-detail">
+              <div class="route-number">18</div>
+              <div class="route-dest">往萬華</div>
+            </div>
+            <div class="route-name">黎忠市場</div>
+            <span class="badge badge-default badge-pill">未發車</span>
           </div>
-          <div class="route-name">黎忠市場</div>
-          <span class="badge badge-default badge-pill">未發車</span>
-        </div>
-        <div class="list-group-item list-group-item-action">
-          <div class="route-detail">
-            <div class="route-number">292</div>
-            <div class="route-dest">往二重</div>
+          <div class="list-group-item list-group-item-action">
+            <div class="route-detail">
+              <div class="route-number">292</div>
+              <div class="route-dest">往二重</div>
+            </div>
+            <div class="route-name">信義通化街口</div>
+            <span class="badge badge-default badge-pill badge-danger">即將抵達</span>
           </div>
-          <div class="route-name">信義通化街口</div>
-          <span class="badge badge-default badge-pill badge-danger">即將抵達</span>
         </div>
       </div>
     </div>
@@ -64,8 +67,11 @@ export default {
   justify-content: space-between;
 }
 
-.content-container {
+.page-container {
   flex: 1;
+}
+
+.content-container {
   overflow: scroll;
   padding-top: .5em;
   padding-bottom: .5em;
